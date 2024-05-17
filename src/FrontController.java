bbb228ef719eb330694b318fd68dd93f7dddbf43
Mamitiana130 <nicoRatimba@gmail.com>
2024-05-17 08:48:58 +0300

    Sprint1_2607

--- src/FrontController.java
+++ src/FrontController.java
@@ -1,4 +1,4 @@
-package servele;
+package servlet;
 
 import java.io.File;
 import java.io.IOException;
@@ -7,59 +7,67 @@ import java.net.URLDecoder;
 import java.util.ArrayList;
 import java.util.List;
 import annotation.*;
+
+import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
 
+import annotation.ControllerAnnotation;
+
 public class FrontController extends HttpServlet {
+    private List<Class<?>> Listecontroller;
+    private boolean isChecked = false;
 
-    private List<String> controllerNames;
+    public void doPost(HttpServletRequest requette, HttpServletResponse response) throws ServletException, IOException {
+        this.processRequest(requette, response);
+    }
 
-    public void init() throws ServletException {
-        String packageToScan = this.getInitParameter("package_name");
+    public void doGet(HttpServletRequest requette, HttpServletResponse response) throws ServletException, IOException {
+        this.processRequest(requette, response);
+    }
 
-        controllerNames = new ArrayList<>();
+    public void processRequest(HttpServletRequest requette, HttpServletResponse response)
+            throws ServletException, IOException {
+        StringBuffer url = requette.getRequestURL();
+        PrintWriter out = response.getWriter();
+        out.println("Mamt was here" + url);
+        if (!this.isChecked) {
+            String packageScan = this.getInitParameter("package_name");
+            try {
+                this.Listecontroller = this.getListeControllers(packageScan);
+                this.isChecked = true;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        for (Class<?> classs : Listecontroller) {
+            out.println(classs.getName());
+        }
+    }
 
-        try {
-            String path = getClass().getClassLoader().getResource(packageToScan.replace('.', '/')).getPath();
-            String decodedPath = URLDecoder.decode(path, "UTF-8");
-            File packageDir = new File(decodedPath);
+    boolean isController(Class<?> c) {
+        return c.isAnnotationPresent(ControllerAnnotation.class);
+    }
 
-            File[] files = packageDir.listFiles();
-            if (files != null) {
-                for (File file : files) {
-                    if (file.isFile() && file.getName().endsWith(".class")) {
-                        String className = packageToScan + "." + file.getName().replace(".class", "");
-                        Class<?> clazz = Class.forName(className);
-                        if (clazz.isAnnotationPresent(ControllerAnnotation.class)) {
-                            controllerNames.add(clazz.getSimpleName());
-                        }
+    List<Class<?>> getListeControllers(String packageName) throws Exception {
+        List<Class<?>> res = new ArrayList<Class<?>>();
+        String path = this.getClass().getClassLoader().getResource(packageName.replace('.', '/')).getPath();
+        String decodedPath = URLDecoder.decode(path, "UTF-8");
+        File packageDir = new File(decodedPath);
+
+        File[] files = packageDir.listFiles();
+        if (files != null) {
+            for (File file : files) {
+                if (file.isFile() && file.getName().endsWith(".class")) {
+                    String className = packageName + "." + file.getName().replace(".class", "");
+                    Class<?> classe = Class.forName(className);
+                    if (this.isController(classe)) {
+                        res.add(classe);
                     }
                 }
             }
-        } catch (IOException | ClassNotFoundException e) {
-            e.printStackTrace();
         }
-    }
+        return res;
 
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-        processedRequest(req, res);
-    }
-
-    protected void doPost(HttpServletRequest req, HttpServletResponse res)
-            throws ServletException, IOException {
-        processedRequest(req, res);
-    }
-
-    protected void processedRequest(HttpServletRequest req, HttpServletResponse res)
-            throws ServletException, IOException {
-        res.setContentType("text/plain");
-        PrintWriter out = res.getWriter();
-        out.println("Tongasoa ato am FrontController");
-        out.println("Votre url : " + req.getRequestURL().toString());
-
-        out.println("Liste des contrôleurs : ");
-        for (String controllerName : controllerNames) {
-            out.println(controllerName);
-        }
     }
 }
