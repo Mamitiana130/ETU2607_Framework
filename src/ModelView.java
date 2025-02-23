5240882f9a8a549fe73ac3b3305aa27e24b24add
Mamitiana130 <155826303+Mamitiana130@users.noreply.github.com>
2025-02-23 10:49:06 +0600

    Sprint14_2607
    Gestion de message d'exception des inputs

--- src/ModelView.java
+++ src/ModelView.java
@@ -1,37 +1,29 @@
-package utils;
+package models;
 
 import java.util.HashMap;
 
-public class ModelView {
+public class ModelView{
     String url;
-    HashMap<String, Object> data;
+    HashMap<String , Object> data;
 
-    public ModelView() {
+    public ModelView(){
         this.data = new HashMap<>();
-    }
-
-    public ModelView(String url, HashMap<String, Object> data) {
-        this.url = url;
-        this.data = data;
-    }
-
-    public String getUrl() {
-        return url;
     }
 
     public void setUrl(String url) {
         this.url = url;
     }
-
+    public String getUrl() {
+        return url;
+    }
+    public void setData(HashMap<String, Object> data) {
+        this.data = data;
+    }
     public HashMap<String, Object> getData() {
         return data;
     }
 
-    public void setData(HashMap<String, Object> data) {
-        this.data = data;
-    }
-
-    public void add(String nameUrl, Object value) {
+    public void add(String nameUrl , Object value){
         this.data.put(nameUrl, value);
     }
 }
--- src/Util.java
+++ src/Util.java
@@ -2,18 +2,26 @@ package utils;
 
 import javax.servlet.http.HttpServletRequest;
 import java.lang.reflect.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import annotation.*;
 import java.lang.Exception;
 import javax.servlet.http.Part;
+import javax.servlet.http.*;
+import javax.servlet.*;
 
 public class Util {
 
-    public static Object[] getParameterValues(HttpServletRequest request, Method method,
+    public static Object[] getParameterValues(HttpServletRequest request,HttpServletResponse response, Method method,
             Class<ParamAnnotation> paramAnnotationClass, Class<ParamObject> paramObjectAnnotationClass)
             throws Exception {
         Parameter[] parameters = method.getParameters();
         Object[] parameterValues = new Object[parameters.length];
+        Map<String, String[]> params = request.getParameterMap();
 
         for (int i = 0; i < parameters.length; i++) {
             if (parameters[i].isAnnotationPresent(paramAnnotationClass)) {
@@ -39,11 +47,43 @@ public class Util {
                 try {
                     Object paramObjectInstance = parameters[i].getType().getDeclaredConstructor().newInstance();
                     Field[] fields = parameters[i].getType().getDeclaredFields();
+                    
+                   Map<String, String> fieldsValues = new HashMap<>();
+                    Map<String, String> validationErrors = new HashMap<>();
+
                     for (Field field : fields) {
                         String fieldName = field.getName();
                         String paramValue = request.getParameter(objName + "." + fieldName);
-                        field.setAccessible(true);
-                        field.set(paramObjectInstance, convertParameterValue(paramValue, field.getType()));
+                        
+                        fieldsValues.put(fieldName, paramValue);
+                        if (paramValue != null) {
+                            field.setAccessible(true);
+                            field.set(paramObjectInstance, convertParameterValue(paramValue, field.getType()));
+                        }
+
+                        String key = objName + "." + fieldName;
+                    }
+                    validationErrors = validateField(paramObjectInstance, params, objName);
+                    
+                    if (!validationErrors.isEmpty()) {
+                        request.setAttribute("errors", validationErrors);
+                        request.setAttribute("values", fieldsValues);
+                        
+                        isError iserr = method.getAnnotation(isError.class);
+                        if(iserr != null) { 
+                            String errUrl = iserr.url();
+                            HttpServletRequest wrappedRequest = new HttpServletRequestWrapper(request) {
+
+                                @Override
+                                public String getMethod() {
+                                    return "GET";
+                                }
+                            };
+                            RequestDispatcher dispatcher = request.getRequestDispatcher(errUrl);
+                            dispatcher.forward(wrappedRequest, response);
+                        }
+                        System.out.println("Validation errors for object: " + objName);
+                        throw new Exception("Validation errors: " + validationErrors);
                     }
                     parameterValues[i] = paramObjectInstance;
                 } catch (Exception e) {
@@ -61,7 +101,88 @@ public class Util {
         return parameterValues;
     }
 
+    public static Map<String, String> validateField(Object obj , Map<String, String[]> params, String objName) throws Exception {
+           
+           Map<String, String> errors =  new HashMap<>();
+           Field[] fields = obj.getClass().getDeclaredFields();
+   
+           for(Field field : fields) {
+   
+               field.setAccessible(true); 
+               String fieldName = field.getName();
+               String key = objName + "." + fieldName; 
+       
+               if (field.isAnnotationPresent(Required.class)) {
+                   if (params.get(key) == null || params.get(key)[0].isEmpty()) {
+                       errors.put(fieldName, "Ce parametre est obligatoire.");   
+                       continue;  
+                   }
+               }
+              
+       
+               if (field.isAnnotationPresent(Length.class)) {
+                   if (params.get(key) != null ) {                 
+                           String valeur = params.get(key)[0].replace(" ", "");
+                           Length length = field.getAnnotation(Length.class);
+   
+                           if (valeur.length() > length.value()) {
+                               errors.put(fieldName,"Ce paramètre ne doit pas dépasser " + length.value() + " caractères.");
+                               continue;
+                           }          
+                   }
+               }
+               
+               if (field.isAnnotationPresent(Numeric.class)) {
+                   if (params.get(key)!= null && !params.get(key)[0].isEmpty()) {                 
+                       try {                    
+                               Double.parseDouble(params.get(key)[0]);
+                       } catch (Exception e) {
+                           System.out.println(e.getMessage());
+                           errors.put(fieldName, "Ce parametre doit etre un nombre.");
+                           continue;
+                       }
+                   }
+               }
+               if (field.isAnnotationPresent(Range.class)) {
+                   if (params.get(key) != null) {
+                       try {
+                           double valeur = Double.parseDouble(params.get(key)[0]);
+                           Range range = field.getAnnotation(Range.class);
+                           if (valeur < range.min() || valeur > range.max()) {
+                               // System.out.println("Ambany na ambony");
+                               errors.put(fieldName, "Ce parametre doit etre entre [" + range.min() + " et " + range.max() + "].");
+                               continue;
+                               // throw new Exception("The parameter " + key + " must be within the range [" + range.min() + ", " + range.max() + "].");
+                           }
+                       } catch (NumberFormatException e) {
+                           errors.put(fieldName, "Ce parametre doit etre un nombre.");
+                           continue;
+                           // throw new Exception("The parameter " + key + " must be a number.");
+                       }
+               
+                   }
+               }
+   
+           }
+           return errors;
+       }
+
     public static Object convertParameterValue(String value, Class<?> type) {
+        if (value == null || value.isEmpty()) {
+            if (type.isPrimitive()) {
+                // Retourner la valeur par défaut pour les types primitifs
+                if (type == int.class) return 0;
+                if (type == boolean.class) return false;
+                if (type == long.class) return 0L;
+                if (type == double.class) return 0.0;
+                if (type == float.class) return 0.0f;
+                if (type == short.class) return (short) 0;
+                if (type == byte.class) return (byte) 0;
+                if (type == char.class) return '\u0000';
+            }
+            // Retourner null pour les types objets
+            return null;
+        }
         if (type == String.class) {
             return value;
         } else if (type == int.class || type == Integer.class) {
